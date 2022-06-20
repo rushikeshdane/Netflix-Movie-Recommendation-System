813eb7da8143fee70456447cba7063fd535914ad
rushikeshdane <78649021+rushikeshdane@users.noreply.github.com>
2022-06-21 00:11:56 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
